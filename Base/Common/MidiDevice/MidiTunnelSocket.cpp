6f2e0ea1f1718a145d0c54ada78985429eaefd96
Ralf Waspe <rwaspe@icloud.com>
2022-04-02 01:01:11 +0200

    debug messages

--- Base/Common/MidiDevice/MidiTunnelSocket.cpp
+++ Base/Common/MidiDevice/MidiTunnelSocket.cpp
@@ -49,7 +49,9 @@ void Midi::Tunnel::Socket::slotIncomingData()
          passthrough->sendBuffer(message);
 
       const Midi::Channel channel = message[0] & 0x0F;
-      if (Midi::Event::NoteOn == (message[0] & 0xF0))
+      const Midi::Event event = static_cast<Midi::Event>(message[0] & 0xF0);
+
+      if (Midi::Event::NoteOn == event)
       {
          const Note note = Note::fromMidi(message[1]);
          const Midi::Velocity velocity = message[2];
@@ -59,7 +61,7 @@ void Midi::Tunnel::Socket::slotIncomingData()
          for (const NoteOnFunction& noteOnFunction : noteOnFunctionList)
             noteOnFunction(channel, note, velocity);
       }
-      else if (Midi::Event::NoteOff == (message[0] & 0xF0))
+      else if (Midi::Event::NoteOff == event)
       {
          const Note note = Note::fromMidi(message[1]);
 
@@ -68,7 +70,7 @@ void Midi::Tunnel::Socket::slotIncomingData()
          for (const NoteOffFunction& noteOffFunction : noteOffFunctionList)
             noteOffFunction(channel, note);
       }
-      else if (Midi::Event::ControlChange == (message[0] & 0xF0))
+      else if (Midi::Event::ControlChange == event)
       {
          const Midi::ControllerMessage controllerMessage = static_cast<Midi::ControllerMessage>(message[1]);
          const uint8_t value = message[2];
