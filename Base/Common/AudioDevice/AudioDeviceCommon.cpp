bdc802e16352683bc57c79bda3dc6491a5d9202f
Ralf Waspe <rwaspe@icloud.com>
2022-08-14 12:18:43 +0200

    move files to lib

--- Base/Common/AudioDevice/AudioDeviceCommon.cpp
+++
@@ -1,3 +0,0 @@
-#include "Private/AudioDeviceCommon.h"
-
-const AudioDevice::Common::SampleRate::StringMap AudioDevice::Common::SampleRate::nameMap = {{Default, "Default"}, {CompactDisk, "CompactDisk"}, {Normal, "Normal"}, {High, "High"}};
--- Base/Common/AudioDevice/AudioDeviceOutputsNote.cpp
+++
@@ -1,6 +0,0 @@
-#include "AudioDeviceOutputsNote.h"
-
-AudioDevice::OutputsNote::OutputsNote()
-{
-}
-
